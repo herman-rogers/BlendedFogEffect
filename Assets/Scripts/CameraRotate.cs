968965358d74cdb4789fd4566d9abc475314165c
Boomer <herman.rogers@gmail.com>
2017-01-11 17:26:11 +0000

    Update main scene and fix broken black screen build

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.RotateAround( Vector3.zero, Vector3.up, 30 * Time.deltaTime );
+    }
+}
